9ff28a17515a4e9bf1e8ee7b6b3e76e4a9cd9cc4
Yago de Jesus Santos <yago_jesus@outlook.com.br>
2023-02-01 13:17:59 -0300

    Upgrade no desafio 1

---
+++ aula1-conversor-de-moedas/js/main.js
@@ -0,0 +1,50 @@
+function conterMoeda(){
+    const btnConverter = document.querySelector("#btn-converter");
+    const valorMoeda = document.querySelector("#valor-moeda");
+    const listaMoeda = document.querySelector("#lista-moedas");
+    var cotacao = 0.0;
+    var opcaoListaMoeda = null;
+    var quantiaMoeda = null;
+
+    listaMoeda.addEventListener("click",()=>{
+        // capturando o valor da opção selecionada
+        opcaoListaMoeda = listaMoeda.options[listaMoeda.selectedIndex].value;
+
+        if(opcaoListaMoeda=="dolar"){
+            
+            valorMoeda.setAttribute("placeholder","$ Valor");
+            cotacao = 5.07;
+
+        }else if(opcaoListaMoeda=="euro"){
+
+            valorMoeda.setAttribute("placeholder","€ Valor");
+            cotacao = 5.53;
+
+        }else if(opcaoListaMoeda=="libra-esterlina"){
+
+            valorMoeda.setAttribute("placeholder","£ Valor");
+            cotacao = 6.25;
+        }else{
+            valorMoeda.setAttribute("placeholder","Moeda...")
+        }
+
+    })
+
+    btnConverter.addEventListener("click",()=>{
+        let saida = "";
+        quantiaMoeda = parseFloat(valorMoeda.value);
+        if(opcaoListaMoeda!=null && quantiaMoeda!=NaN && quantiaMoeda!=null && quantiaMoeda>0){
+            let valorConvertido = cotacao*quantiaMoeda;
+            saida = `A converção em real é igual à: R$${valorConvertido.toFixed(2)}\nÚltima data de atualização das cotações: 01/02/2023`;
+            // limpando campo de texto e resetando a lista para a primeira opção
+            valorMoeda.value="";
+            listaMoeda.selectedIndex=0;
+        } else{
+            saida = `Preencha os campos com valores válidos!`
+        }
+        alert(saida);
+        
+    })
+
+    
+}
